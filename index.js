a84385e12599f3e75ec7f1338ecfc34d97156abf
Hunter <kolasa.hunter@aol.com>
2021-03-04 14:10:36 -0500

    commit tree cce8cdd50f1ea5861a7b170e5ada8b1bc51d2d33

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(array, fn) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        let e = array[i]
+        newArray.push(fn(e))
+    }
+    return newArray
+}
+
+function reduce(array, fn, startingPoint) {
+    let r = (!!startingPoint) ? startingPoint : array[0];
+    let i = (!!startingPoint) ? 0 : 1
+    for (; i < array.length; i++) {
+        r = fn(array[i], r)
+    }
+    return r
+}
